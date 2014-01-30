3eef321edf02173de2c77397c5f97eadb07ee3ea
Lacyg <lacyg@cs2.ch>
2014-01-30 12:21:06 +0100

    Update SitemapTxtPage.php
    class name fix

--- Classes/Page/SitemapTxtPage.php
+++ Classes/Page/SitemapTxtPage.php
@@ -32,7 +32,7 @@ namespace TQ\TqSeo\Page;
  * @subpackage  Page
  * @version     $Id: class.robots_txt.php 62700 2012-05-22 15:53:22Z mblaschke $
  */
-class SitemapXmlPage extends \TQ\TqSeo\Page\AbstractPage {
+class SitemapTxtPage extends \TQ\TqSeo\Page\AbstractPage {
 
     // ########################################################################
     // Attributes
